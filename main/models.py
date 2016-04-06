3c0c4b746817ea393ad12d782b283e59d488a5b1
Шамрай Денис < >
2016-04-06 17:28:05 +0300

    added models and fixtures

--- main/models.py
+++ main/models.py
@@ -1,5 +1,23 @@
 from __future__ import unicode_literals
-
 from django.db import models
+import json
 
-# Create your models here.
+
+class Good(models.Model):
+    title = models.CharField(max_length=255)
+    short = models.TextField()
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    options_text = models.TextField()
+    details = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def get_options(self):
+        return json.loads(self.options_text)
+
+
+class Picture(models.Model):
+    good = models.ForeignKey(Good, related_name="pictures")
+    url = models.URLField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
